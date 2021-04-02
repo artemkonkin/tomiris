76c833d6720dc2dcac9015ee9006eb340ef3669f
Artem Konkin <temakonkin@gmail.com>
2021-04-02 16:15:43 +0600

    test

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace tomiris.Controllers
 {
@@ -6,6 +8,8 @@ namespace tomiris.Controllers
     {
         public IActionResult Index()
         {
+            TestFunctions tFun = new();
+            tFun.TcDictionary();
             return View();
         }
 
@@ -14,6 +18,42 @@ namespace tomiris.Controllers
             ViewData["Message"] = "Hello " + name;
             ViewData["NumTimes"] = numTimes;
             return View();
+        }
+    }
+
+    public class TestFunctions : Debugging
+    {
+        private string value;
+
+        public TestFunctions()
+        {
+
+        }
+
+        public int TcDictionary()
+        {
+            DgMessage("Test Class Dictionary");
+            Dictionary<string, string> dicSet = new Dictionary<string, string>();
+            dicSet.Add("Namba","Altynay Abdieva");
+            dicSet.Add("Ooba","Artem Konkin");
+            dicSet.Add("Krista","Begimay Abdieva");
+            if(dicSet.TryGetValue("Namba",out value))
+            {
+                DgMessage($"Namba {value}");
+            }
+            else
+            {
+                DgMessage("Not found");
+            }
+            return 0;
+        }
+    }
+
+    public class Debugging
+    {
+        public void DgMessage(string message)
+        {
+            Debug.WriteLine($"Debug: {message}");
         }
     }
 }
